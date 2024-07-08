a2ae2d7ffc4d96a01e48e80f506d7fa10d350a7e
Dimitrios Athanasopoulos <138149005+Griego86@users.noreply.github.com>
2024-07-08 13:36:25 -0700

    Refactor function to include sets

--- random_cards/poker_cards.py
+++ random_cards/poker_cards.py
@@ -4,11 +4,25 @@ suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
 faces = ["Jack", "Queen", "King", "Ace"]
 numbered = [2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-def draw():
-  the_suit = random.choice(suits)
-  the_card = random.choice(faces + numbered)
-  return the_card, "of", the_suit
+# def draw():
+#   the_suit = random.choice(suits)
+#   the_card = random.choice(faces + numbered)
+#   return the_card, "of", the_suit
 
-print(draw())
-print(draw())
-print(draw())
+# print(draw())
+# print(draw())
+# print(draw())
+
+# Re-write as set
+deck = set()
+
+for suit in suits:
+  for card in faces + numbered:
+    deck.add((card, "of", suit))
+
+def draw():
+  card = random.choice(list(deck))
+  deck.remove(card)
+  return card
+
+print(draw(), len(deck))
